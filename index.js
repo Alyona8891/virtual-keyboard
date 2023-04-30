720de732452f444bc074dc70c5ed22cfa6bc7e58
unknown <besssta888@mail.ru>
2023-04-30 17:57:28 +0300

    feat: refine function createBtn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const keyboard = [{ row1: '~', row2: '`' }, { row1: '!', row2: '1' }, { row1: '@', row2: '2' }, { row1: '#', row2: '3' }, { row1: '$', row2: '4' }, { row1: '%', row2: '5' }, { row1: '^', row2: '6' }, { row1: '&', row2: '7' }, { row1: '*', row2: '8' }, { row1: '(', row2: '9' }, { row1: ')', row2: '0' }, { row1: '_', row2: '-' }, { row1: '+', row2: '=' }, { row1: null, row2: 'BACKSPACE' }, { row1: null, row2: 'TAB' }, { row1: null, row2: 'Q' }, { row1: null, row2: 'W' }, { row1: null, row2: 'E' }, { row1: null, row2: 'R' }, { row1: null, row2: 'T' }, { row1: null, row2: 'Y' }, { row1: null, row2: 'U' }, { row1: null, row2: 'I' }, { row1: null, row2: 'O' }, { row1: null, row2: 'P' }, { row1: null, row2: '[' }, { row1: null, row2: ']' }, { row1: '|', row2: '\\' }, { row1: null, row2: 'DEL' }, { row1: null, row2: 'CAPS LOCK' }, { row1: null, row2: 'A' }, { row1: null, row2: 'S' }, { row1: null, row2: 'D' }, { row1: null, row2: 'F' }, { row1: null, row2: 'G' }, { row1: null, row2: 'H' }, { row1: null, row2: 'J' }, { row1: null, row2: 'K' }, { row1: null, row2: 'L' }, { row1: null, row2: ';' }, { row1: '"', row2: '\'' }, { row1: null, row2: 'ENTER' }, { row1: null, row2: 'SHIFT' }, { row1: null, row2: '\\' }, { row1: null, row2: 'Z' }, { row1: null, row2: 'X' }, { row1: null, row2: 'C' }, { row1: null, row2: 'V' }, { row1: null, row2: 'B' }, { row1: null, row2: 'N' }, { row1: null, row2: 'M' }, { row1: null, row2: '.' }, { row1: null, row2: ',' }, { row1: null, row2: '/' }, { row1: null, row2: '▲' }, { row1: null, row2: 'SHIFT' }, { row1: null, row2: 'CTRL' }, { row1: null, row2: 'WIN' }, { row1: null, row2: 'ALT' }, { row1: null, row2: null }, { row1: null, row2: 'ALT' }, { row1: null, row2: 'CTRL' }, { row1: null, row2: '◀' }, { row1: null, row2: '▼' }, { row1: null, row2: '▶' }];
+const keyboard = [{ row1: '~', row2: '`' }, { row1: '!', row2: '1' }, { row1: '@', row2: '2' }, { row1: '#', row2: '3' }, { row1: '$', row2: '4' }, { row1: '%', row2: '5' }, { row1: '^', row2: '6' }, { row1: '&', row2: '7' }, { row1: '*', row2: '8' }, { row1: '(', row2: '9' }, { row1: ')', row2: '0' }, { row1: '_', row2: '-' }, { row1: '+', row2: '=' }, { row1: null, row2: 'BACKSPACE' }, { row1: null, row2: 'TAB' }, { row1: null, row2: 'Q' }, { row1: null, row2: 'W' }, { row1: null, row2: 'E' }, { row1: null, row2: 'R' }, { row1: null, row2: 'T' }, { row1: null, row2: 'Y' }, { row1: null, row2: 'U' }, { row1: null, row2: 'I' }, { row1: null, row2: 'O' }, { row1: null, row2: 'P' }, { row1: null, row2: '[' }, { row1: null, row2: ']' }, { row1: '|', row2: '\\' }, { row1: null, row2: 'DEL' }, { row1: null, row2: 'CAPS LOCK' }, { row1: null, row2: 'A' }, { row1: null, row2: 'S' }, { row1: null, row2: 'D' }, { row1: null, row2: 'F' }, { row1: null, row2: 'G' }, { row1: null, row2: 'H' }, { row1: null, row2: 'J' }, { row1: null, row2: 'K' }, { row1: null, row2: 'L' }, { row1: null, row2: ';' }, { row1: '"', row2: '\'' }, { row1: null, row2: 'ENTER' }, { row1: null, row2: 'SHIFT' }, { row1: null, row2: '\\' }, { row1: null, row2: 'Z' }, { row1: null, row2: 'X' }, { row1: null, row2: 'C' }, { row1: null, row2: 'V' }, { row1: null, row2: 'B' }, { row1: null, row2: 'N' }, { row1: null, row2: 'M' }, { row1: null, row2: '.' }, { row1: null, row2: ',' }, { row1: null, row2: '/' }, { row1: null, row2: '▲' }, { row1: null, row2: 'SHIFT' }, { row1: null, row2: 'CTRL' }, { row1: null, row2: 'WIN' }, { row1: null, row2: 'ALT' }, { row1: null, row2: ' ' }, { row1: null, row2: 'ALT' }, { row1: null, row2: 'CTRL' }, { row1: null, row2: '◄' }, { row1: null, row2: '▼' }, { row1: null, row2: '►' }];
 
 document.body.className = 'page';
 const main = document.createElement('main');
@@ -22,7 +22,7 @@ keyboardBlock.append(keyboardInnerBlock);
 
 }*/
 
-function createBtn(a, b, classBlockColor, classTopColor, classBlockSize, classTopSize) {
+function createBtn(a, b, classBlockColor, classTopColor, classBlockSize, classTopSize, capsLock) {
   const btnBlock = document.createElement('div');
   btnBlock.className = `keyboard-block__btn btn ${classBlockSize} ${classBlockColor}`;
   const btnTopBlock = document.createElement('div');
@@ -38,7 +38,35 @@ function createBtn(a, b, classBlockColor, classTopColor, classBlockSize, classTo
   btnBlockRow1.innerText = a;
   btnBlockRow2.innerText = b;
   btnTopBlock.append(btnBlockRow1);
+  if (capsLock) {
+    const capsLockMarker = document.createElement('div');
+    capsLockMarker.className = 'btn__marker';
+    btnBlockRow1.append(capsLockMarker);
+  }
   btnTopBlock.append(btnBlockRow2);
+  btnBlock.addEventListener('click', function my() {
+    textAreaBlock.focus();
+    const key = btnBlockRow2.textContent;
+    if (key === 'BACKSPACE') {
+      const position = textAreaBlock.selectionStart;
+      textAreaBlock.value = textAreaBlock.value.slice(0, position - 1) + textAreaBlock.value.slice(position);
+      textAreaBlock.selectionStart = position - 1;
+      textAreaBlock.selectionEnd = position - 1;
+    } else if (key === 'DEL') {
+      const position = textAreaBlock.selectionStart;
+      textAreaBlock.value = textAreaBlock.value.slice(0, position) + textAreaBlock.value.slice(position + 1);
+      textAreaBlock.selectionStart = position;
+      textAreaBlock.selectionEnd = position;
+    } else if (key === 'ENTER') {
+      textAreaBlock.value += '\n';
+    } else if (key === 'TAB') {
+      textAreaBlock.value += '  ';
+    } else if (key === 'SHIFT' || key === 'ALT' || key === 'CTRL' || key === 'WIN' || key === 'CAPS LOCK') {
+      console.log();
+    } else {
+      textAreaBlock.value += key.toLowerCase();
+    }
+  })
   return btnBlock;
 }
 for (let i = 0; i < keyboard.length; i += 1) {
@@ -51,7 +79,7 @@ for (let i = 0; i < keyboard.length; i += 1) {
   } else if (i === 28 || i === 55 || i === 57 || i === 58 || i === 60) {
     keyboardInnerBlock.append(createBtn(keyboard[i].row1, keyboard[i].row2, 'btn_blue', 'btn__top-block_blue'));
   } else if (i === 29) {
-    keyboardInnerBlock.append(createBtn(keyboard[i].row1, keyboard[i].row2, 'btn_orange', 'btn__top-block_orange', 'btn_backspace-size', 'btn__top-block_backspace-size'));
+    keyboardInnerBlock.append(createBtn(keyboard[i].row1, keyboard[i].row2, 'btn_orange', 'btn__top-block_orange', 'btn_backspace-size', 'btn__top-block_backspace-size', 'capsLock'));
   } else if (i === 41) {
     keyboardInnerBlock.append(createBtn(keyboard[i].row1, keyboard[i].row2, 'btn_orange', 'btn__top-block_orange', 'btn_enter-size', 'btn__top-block_enter-size'));
   } else if (i === 59) {

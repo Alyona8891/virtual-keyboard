e7bb7bb86c8240c0fd9ff995588251f94f409002
unknown <besssta888@mail.ru>
2023-05-02 04:02:00 +0300

    fix: correct mistakes

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
-import {keyboardEn, keyboardRu, keyboardEnShift, keyboardRuShift} from './components/layots.js';
-import {createBtn} from './components/button.js';
-import {changeTextareaValue, textAreaBlock} from './components/change-textarea-value.js';
+import {
+  keyboardEn, keyboardRu, keyboardEnShift, keyboardRuShift,
+} from './components/layots.js';
+import {
+  createBtn,
+} from './components/button.js';
+import {
+  changeTextareaValue, textAreaBlock,
+} from './components/change-textarea-value.js';
 
 let resultArr = [];
+let capsLockNum = false;
 
 function createKeyboard(arr) {
   document.body.className = 'page';
   const main = document.createElement('main');
   main.className = 'page__main-page main-page';
   document.body.prepend(main);
-  if(!localStorage.lang) {
+  if (!localStorage.lang) {
     localStorage.setItem('lang', 'en');
   }
   textAreaBlock.className = 'main-page__textarea-block textarea-block';
@@ -32,7 +39,7 @@ function createKeyboard(arr) {
   for (let i = 0; i < arr.length; i += 1) {
     if (i === 0 || i === 53 || i === 61 || i === 62 || i === 60) {
       keyboardInnerBlock.append(createBtn(arr[i].symbol, arr[i].keyCode, 'btn_orange', 'btn__top-block_orange'));
-    } else if (i === 13 ) {
+    } else if (i === 13) {
       keyboardInnerBlock.append(createBtn(arr[i].symbol, arr[i].keyCode, 'btn_blue', 'btn__top-block_blue', 'btn_backspace-size', 'btn__top-block_backspace-size'));
     } else if (i === 14 || i === 55 || i === 63) {
       keyboardInnerBlock.append(createBtn(arr[i].symbol, arr[i].keyCode, 'btn_blue', 'btn__top-block_blue', 'btn_tab-size', 'btn__top-block_tab-size'));
@@ -53,162 +60,204 @@ function createKeyboard(arr) {
     }
   }
 }
-
-document.addEventListener('keydown', changeKeydown);
-
-function changeKeydown (event) {
+function changeKeydown(event) {
   event.preventDefault();
   const arrBtn = document.querySelectorAll('.btn');
   arrBtn.forEach((el) => {
-    if(event.code.toLowerCase() === el.id) {
+    if (event.code.toLowerCase() === el.id) {
       el.classList.add('btn_active');
       changeTextareaValue(el.innerText);
     }
-  })
+  });
 }
 
-document.addEventListener('keyup', changeKeyUp);
-
-function changeKeyUp (event) {
+function changeKeyUp(event) {
   const arrBtn = document.querySelectorAll('.btn');
   arrBtn.forEach((el) => {
-    if(event.code.toLowerCase() === el.id) {
+    if (event.code.toLowerCase() === el.id) {
       el.classList.remove('btn_active');
     }
-  })
+  });
 }
-createResultArr();
-createKeyboard(resultArr);
-const capsLock = document.querySelector('#capslock');
-capsLock.addEventListener('click', handlerCapsLock);
-let capsLockNum = false;
 
 function createResultArr() {
-  localStorage.lang === 'en'? resultArr = keyboardEn : resultArr = keyboardRu;
+  if (localStorage.lang === 'en') {
+    resultArr = keyboardEn;
+  } else {
+    resultArr = keyboardRu;
+  }
+}
+
+function renameBtns(arr) {
+  const arrElems = document.querySelectorAll('.btn__top-block');
+
+  arrElems.forEach((el, i) => {
+    const elemCapsLock = document.querySelector('#capslock');
+    if (el.parentNode !== elemCapsLock) {
+      const k = el;
+      k.innerText = arr[i].symbol;
+    }
+  });
 }
 
 function handlerCapsLock() {
   const btnMarker = document.querySelector('.btn__marker');
   btnMarker.classList.toggle('btn__marker_active');
   createResultArr();
-  if(!capsLockNum) {  
-  resultArr.forEach(el => {
-    let symbolProperty = el.symbol;
-    el.symbol = symbolProperty.toUpperCase();
-  });
-  renameBtns(resultArr);
-  capsLockNum = true;
+  if (!capsLockNum) {
+    resultArr.forEach((el) => {
+      const k = el;
+      const symbolProperty = el.symbol;
+      k.symbol = symbolProperty.toUpperCase();
+    });
+    renameBtns(resultArr);
+    capsLockNum = true;
   } else {
-    resultArr.forEach(el => {
-      let symbolProperty = el.symbol;
+    resultArr.forEach((el) => {
+      const symbolProperty = el.symbol;
       if (el.symbol.length === 1) {
-        el.symbol = symbolProperty.toLowerCase();
+        const k = el;
+        k.symbol = symbolProperty.toLowerCase();
       }
     });
-    renameBtns(resultArr)
+    renameBtns(resultArr);
     capsLockNum = false;
   }
 }
+function handlerShiftBtn1() {
+  if (localStorage.lang === 'en') {
+    resultArr = keyboardEnShift;
+  } else if (localStorage.lang === 'ru') {
+    resultArr = keyboardRuShift;
+  }
+  resultArr.forEach((el) => {
+    const symbolProperty = el.symbol;
+    if (el.symbol.length === 1 && capsLockNum === false) {
+      const k = el;
+      k.symbol = symbolProperty.toUpperCase();
+    } else if (el.symbol.length === 1 && capsLockNum === true) {
+      const k = el;
+      k.symbol = symbolProperty.toLowerCase();
+    }
+  });
+  renameBtns(resultArr);
+}
+
+function handlerShiftBtn2() {
+  if (localStorage.lang === 'en') {
+    resultArr = keyboardEn;
+  } else {
+    resultArr = keyboardRu;
+  }
+  resultArr.forEach((el) => {
+    const symbolProperty = el.symbol;
+    if (el.symbol.length === 1 && capsLockNum === false) {
+      const k = el;
+      k.symbol = symbolProperty.toLowerCase();
+    } else if (el.symbol.length === 1 && capsLockNum === true) {
+      const k = el;
+      k.symbol = symbolProperty.toUpperCase();
+    }
+  });
+  renameBtns(resultArr);
+}
+
+document.addEventListener('keydown', changeKeydown);
+
+document.addEventListener('keyup', changeKeyUp);
+
+createResultArr();
+createKeyboard(resultArr);
+const capsLock = document.querySelector('#capslock');
+capsLock.addEventListener('click', handlerCapsLock);
 
 function runCtrlShift(func, ...codes) {
+  const pressed = new Set();
 
-  let pressed = new Set();
-
-  document.addEventListener('keydown', function(event) {
+  document.addEventListener('keydown', (event) => {
     pressed.add(event.code);
-    for (let code of codes) { 
+    let hasCode = true;
+    codes.forEach((code) => {
       if (!pressed.has(code)) {
-        return;
+        hasCode = false;
       }
+    });
+    if (hasCode) {
+      pressed.clear();
+      func();
     }
-    pressed.clear();
-    func();
   });
-  document.addEventListener('keyup', function(event) {
+  document.addEventListener('keyup', (event) => {
     pressed.delete(event.code);
   });
 }
 
-function renameBtns(arr) {
-
-  let arrElems = document.querySelectorAll('.btn__top-block');
-
-  arrElems.forEach((el, i) => {
-    let elemCapsLock = document.querySelector('#capslock')
-    if(el.parentNode !== elemCapsLock) {
-      el.innerText = arr[i].symbol;
-    }
-  })
-
-}
-
 function changeLanguage() {
-
   if (localStorage.getItem('lang') === 'en') {
     resultArr = keyboardRu;
-    let arrElems = document.querySelectorAll('.btn__top-block');
+    const arrElems = document.querySelectorAll('.btn__top-block');
     arrElems.forEach((el, i) => {
-    
-    let elemCapsLock = document.querySelector('#capslock')
-      if(el.parentNode !== elemCapsLock) {
-        if(el.innerText.length === 1 && capsLockNum) {
-          el.innerText = resultArr[i].symbol.toUpperCase();
+      const elemCapsLock = document.querySelector('#capslock');
+      if (el.parentNode !== elemCapsLock) {
+        if (el.innerText.length === 1 && capsLockNum) {
+          const k = el;
+          k.innerText = resultArr[i].symbol.toUpperCase();
         } else if (el.innerText.length === 1 && !capsLockNum) {
-          el.innerText = resultArr[i].symbol.toLowerCase();
+          const k = el;
+          k.innerText = resultArr[i].symbol.toLowerCase();
         }
       }
-    })
+    });
     localStorage.lang = 'ru';
   } else {
     resultArr = keyboardEn;
-    let arrElems = document.querySelectorAll('.btn__top-block');
+    const arrElems = document.querySelectorAll('.btn__top-block');
     arrElems.forEach((el, i) => {
-    let elemCapsLock = document.querySelector('#capslock')
-      if(el.parentNode !== elemCapsLock) {
-        if(el.innerText.length === 1 && capsLockNum) {
-          el.innerText = resultArr[i].symbol.toUpperCase();
+      const elemCapsLock = document.querySelector('#capslock');
+      if (el.parentNode !== elemCapsLock) {
+        if (el.innerText.length === 1 && capsLockNum) {
+          const k = el;
+          k.innerText = resultArr[i].symbol.toUpperCase();
         } else if (el.innerText.length === 1 && !capsLockNum) {
-          el.innerText = resultArr[i].symbol.toLowerCase();
+          const k = el;
+          k.innerText = resultArr[i].symbol.toLowerCase();
         }
       }
-    })
+    });
     localStorage.lang = 'en';
   }
-
 }
 
 function handlerShift(code) {
-
-  document.addEventListener('keydown', function(event) {
-    
+  document.addEventListener('keydown', (event) => {
     if (event.code !== code) {
-        return;
+      return;
     }
     resultArr = keyboardEnShift;
-    resultArr.forEach(el => {
-    let symbolProperty = el.symbol;
-    if (el.symbol.length === 1) {
-      el.symbol = symbolProperty.toUpperCase();
-    }
-  });
+    resultArr.forEach((el) => {
+      const symbolProperty = el.symbol;
+      if (el.symbol.length === 1) {
+        const k = el;
+        k.symbol = symbolProperty.toUpperCase();
+      }
+    });
     renameBtns(resultArr);
   });
 
-  document.addEventListener('keyup', function(event) {
-
+  document.addEventListener('keyup', (event) => {
     if (event.code !== code) {
       return;
-  }
-    resultArr = keyboardEn;
-    resultArr.forEach(el => {
-    let symbolProperty = el.symbol;
-    if (el.symbol.length === 1) {
-      el.symbol = symbolProperty.toLowerCase();
     }
+    resultArr = keyboardEn;
+    resultArr.forEach((el) => {
+      const symbolProperty = el.symbol;
+      if (el.symbol.length === 1) {
+        const k = el;
+        k.symbol = symbolProperty.toLowerCase();
+      }
+    });
+    renameBtns(resultArr);
   });
-  renameBtns(resultArr);
-  });
-
 }
 
 runCtrlShift(changeLanguage, 'ControlLeft', 'AltLeft');
@@ -221,38 +270,3 @@ shiftleft.addEventListener('mousedown', handlerShiftBtn1);
 shiftleft.addEventListener('mouseup', handlerShiftBtn2);
 shiftright.addEventListener('mousedown', handlerShiftBtn1);
 shiftright.addEventListener('mouseup', handlerShiftBtn2);
-
-function handlerShiftBtn1() { 
-
-  if(localStorage.lang === 'en') {
-    resultArr = keyboardEnShift;
-  } else if (localStorage.lang === 'ru') {
-    resultArr = keyboardRuShift;
-  }
-  resultArr.forEach(el => {
-    let symbolProperty = el.symbol;
-    if (el.symbol.length === 1 && capsLockNum === false) {
-      el.symbol = symbolProperty.toUpperCase();
-    } else if(el.symbol.length === 1 && capsLockNum === true) {
-      el.symbol = symbolProperty.toLowerCase();
-    }
-  });
-  renameBtns(resultArr);
-}
-function handlerShiftBtn2() { 
-
-  if(localStorage.lang === 'en') {
-    resultArr = keyboardEn;
-  } else {
-    resultArr = keyboardRu;
-  }
-  resultArr.forEach(el => {
-    let symbolProperty = el.symbol;
-    if (el.symbol.length === 1 && capsLockNum === false) {
-      el.symbol = symbolProperty.toLowerCase();
-    } else if(el.symbol.length === 1 && capsLockNum === true) {
-      el.symbol = symbolProperty.toUpperCase();
-    }
-  });
-  renameBtns(resultArr);
-}

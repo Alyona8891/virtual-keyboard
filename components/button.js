c6b914728ce5aa22a8876d0c51c75d1d653584e7
unknown <besssta888@mail.ru>
2023-05-02 04:53:37 +0300

    fix: correct mistakes

--- components/button.js
+++ components/button.js
@@ -1,41 +1,47 @@
-import {btnBlockHandler} from './button-block-handler.js';
+import { btnBlockHandler } from './button-block-handler.js';
 
 const btnClickSound = new Audio();
 btnClickSound.src = './source/2.mp3';
 btnClickSound.volume = 0.05;
 
-export function createBtn(a, c, classBlockColor, classTopColor, classBlockSize, classTopSize, capsLock) {
+export function createBtn(
+  a,
+  c,
+  classBlockColor,
+  classTopColor,
+  classBlockSize,
+  classTopSize,
+  capsLock,
+) {
+  const btnBlock = document.createElement('div');
+  btnBlock.className = 'keyboard-block__btn btn';
+  const btnTopBlock = document.createElement('div');
+  btnTopBlock.className = 'btn__top-block';
 
-    const btnBlock = document.createElement('div');
-    btnBlock.className = 'keyboard-block__btn btn';
-    const btnTopBlock = document.createElement('div');
-    btnTopBlock.className = 'btn__top-block';
-  
-    btnBlock.id = c;
-  
-    if (classBlockSize) {
-      btnBlock.classList.add(classBlockSize);
-    }
-    if (classBlockColor) {
-      btnBlock.classList.add(classBlockColor);
-    }
-    if (classTopSize) {
-      btnTopBlock.classList.add(classTopSize);
-    }
-    if (classTopColor) {
-      btnTopBlock.classList.add(classTopColor);
-    }
-    btnBlock.append(btnTopBlock);
-    btnTopBlock.innerText = a;
-  
-    if (capsLock) {
-      const capsLockMarker = document.createElement('div');
-      capsLockMarker.className = 'btn__marker';
-      btnTopBlock.append(capsLockMarker);
-    }
-  
-    btnBlock.addEventListener('click', btnBlockHandler);
-    btnBlock.addEventListener('click', () => btnClickSound.play());
-    return btnBlock;
+  btnBlock.id = c;
+
+  if (classBlockSize) {
+    btnBlock.classList.add(classBlockSize);
+  }
+  if (classBlockColor) {
+    btnBlock.classList.add(classBlockColor);
+  }
+  if (classTopSize) {
+    btnTopBlock.classList.add(classTopSize);
+  }
+  if (classTopColor) {
+    btnTopBlock.classList.add(classTopColor);
+  }
+  btnBlock.append(btnTopBlock);
+  btnTopBlock.innerText = a;
+
+  if (capsLock) {
+    const capsLockMarker = document.createElement('div');
+    capsLockMarker.className = 'btn__marker';
+    btnTopBlock.append(capsLockMarker);
   }
 
+  btnBlock.addEventListener('click', btnBlockHandler);
+  btnBlock.addEventListener('click', () => btnClickSound.play());
+  return btnBlock;
+}
